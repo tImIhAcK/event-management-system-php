ba9844eda6f8178e5d4dd5ba74d790c0c5dffbd0
Adeniran John <adeniranjohn2016@gmail.com>
2024-01-23 15:39:07 +0100

    updated

--- includes/constants.php
+++ includes/constants.php
@@ -2,7 +2,7 @@
 
 session_start();
 
-define('BASE_URL', 'http://locahost/event-management-system/');
+define('BASE_URL', 'http://localhost/event-management-system/');
 define('ROOT_PATH', realpath(dirname(__FILE__)));
 define('DB_NAME', 'EMS');
 define('DB_HOST', 'localhost');
